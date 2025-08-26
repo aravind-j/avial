9b5fc7bd44b0a74bf9f6ab3545f7c0992d50f2f0
aravind-j <aravindjayaramanagri@gmail.com>
2025-08-27 00:25:28 +0530

    update

--- R/globals.R
+++ R/globals.R
@@ -2,7 +2,8 @@
 if (getRversion() >= "2.15.1")  {
   utils::globalVariables(
     c(
-    "prop_gp", "prop_trt", "count", ":=", "se", "Value", "coresize", "method"
+    "prop_gp", "prop_trt", "count", ":=", "se", "Value", "coresize", "method",
+    "..cols"
     )
   )
 }

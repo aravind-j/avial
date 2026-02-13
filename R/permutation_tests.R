9376791818cca16af99c7f65ef01884a5c486d3c
aravind-j <aravindjayaramanagri@gmail.com>
2026-02-13 09:33:41 +0530

    Update to handle invalid permutations

--- R/permutation_tests.R
+++ R/permutation_tests.R
@@ -15,6 +15,11 @@
 #'   have the same length as \code{x}.
 #' @param fun A function to summarize values within each group.
 #' @param R Integer specifying the number of permutations. Default is 1000.
+#' @param max_invalid Numeric between 0 and 1. Maximum allowed proportion of
+#'   invalid permutations (i.e., permutations for which the test statistic is
+#'   non-finite). If the proportion of invalid permutations exceeds this
+#'   threshold, the function stops execution with an error, indicating that the
+#'   statistic function is not permutation-stable.
 #' @param p.adjust.method (perm.test.pairwise only) Method for adjusting
 #'   p-values for multiple comparisons. Options include \code{"bonferroni"} and
 #'   \code{"holm"}. Default is \code{"bonferroni"}.
@@ -76,6 +81,7 @@ NULL
 #' @rdname permutation_tests
 #' @export
 perm.test.global <- function(x, group, fun, R = 1000,
+                             max.invalid = 0.25,
                              parallel = c("no", "multicore", "snow"),
                              ncpus = 1L,
                              cl = NULL,
@@ -86,6 +92,11 @@ perm.test.global <- function(x, group, fun, R = 1000,
   stopifnot(length(x) == length(group))
   stopifnot(is.factor(x) || is.numeric(x))
   stopifnot(is.factor(group))
+
+  if (!is.numeric(max.invalid) || length(max.invalid) != 1L ||
+      max.invalid <= 0 || max.invalid >= 1) {
+    stop("'max.invalid' must be a single number between 0 and 1.", call. = FALSE)
+  }
 
   group <- droplevels(group)
 
@@ -113,8 +124,18 @@ perm.test.global <- function(x, group, fun, R = 1000,
   # test_stat <-  sum(obs_indices^2 * counts)
 
   # permutation loop to get null variance
-  null_stats <-
-    apply_parallel(seq_len(R), function(i) {
+  null_stats <- numeric(R)
+  n_valid <- 0L
+
+  n_invalid_total <- 0
+  n_generated_total <- 0
+
+  while (n_valid < R) {
+
+    batch_size <- min(R - n_valid, R)
+
+    # permutation loop to get null variance
+    batch <- apply_parallel(seq_len(batch_size), function(i) {
       shuffled_group <- sample(group)
       perm_indices <- tapply(x, shuffled_group, fun, ...)
 
@@ -127,11 +148,64 @@ perm.test.global <- function(x, group, fun, R = 1000,
 
       perm_grand_mean <- sum(perm_indices * perm_counts) / sum(perm_counts)
 
-      # var(perm_indices)
-      sum(perm_counts * ((perm_indices - perm_grand_mean) ^ 2))
-      # sum(perm_indices^2 * perm_counts)
+      # stat <- var(perm_indices)
+      stat <- sum(perm_counts * ((perm_indices - perm_grand_mean) ^ 2))
+      # stat <- sum(perm_indices^2 * perm_counts)
+
+      if (!is.finite(stat)) {
+        return(NA_real_)
+      } else {
+        stat
+      }
+
     }, parallel = parallel, ncpus = ncpus, cl = cl)
-  null_stats <- sapply(null_stats, identity)
+
+    batch <- unlist(batch)
+
+    n_generated_total <- n_generated_total + length(batch)
+    n_invalid_total   <- n_invalid_total + sum(!is.finite(batch))
+
+    # EARLY STOP CHECK
+    if (n_generated_total > 0) {
+      current_prop_invalid <- n_invalid_total / n_generated_total
+
+      if (current_prop_invalid > max.invalid) {
+        stop(
+          sprintf(
+            "Permutation test aborted: statistic function '%s' is not permutation-stable (%.1f%% invalid permutations, threshold = %.0f%%).",
+            deparse(substitute(fun)),
+            100 * current_prop_invalid,
+            100 * max.invalid
+          ),
+          call. = FALSE
+        )
+      }
+    }
+
+    batch <- batch[is.finite(batch)] # check if valid
+
+    if (length(batch) > 0) {
+      idx <- seq.int(from = n_valid + 1L,
+                     length.out = length(batch))
+      null_stats[idx] <- batch
+      n_valid <- n_valid + length(batch)
+    }
+  }
+
+  # null_stats <- null_stats[seq_len(R)]
+
+  if (n_generated_total > 0) {
+    prop_invalid <- n_invalid_total / n_generated_total
+
+    if (prop_invalid > 0.02) {
+      warning(
+        sprintf("Function '%s' is not permutation-stable (%.1f%% invalid permutations).",
+                deparse(substitute(fun)),
+                100 * prop_invalid),
+        call. = FALSE
+      )
+    }
+  }
 
   # Calculate P-value
   # phipson_permutation_2010
@@ -150,6 +224,7 @@ perm.test.global <- function(x, group, fun, R = 1000,
 #' @export
 perm.test.pairwise <- function(x, group, fun, R = 1000,
                                p.adjust.method = c("bonferroni", "holm"),
+                               max.invalid = 0.25,
                                parallel = c("no", "multicore", "snow"),
                                ncpus = 1L,
                                cl = NULL,
@@ -162,6 +237,11 @@ perm.test.pairwise <- function(x, group, fun, R = 1000,
   stopifnot(length(x) == length(group))
   stopifnot(is.factor(x) || is.numeric(x))
   stopifnot(is.factor(group))
+
+  if (!is.numeric(max.invalid) || length(max.invalid) != 1L ||
+      max.invalid <= 0 || max.invalid >= 1) {
+    stop("'max.invalid' must be a single number between 0 and 1.", call. = FALSE)
+  }
 
   group <- droplevels(group)
 
@@ -184,19 +264,94 @@ perm.test.pairwise <- function(x, group, fun, R = 1000,
     mask <- group %in% p
     sub_x <- x[mask]
     sub_g <- droplevels(group[mask])
+
     # Pairwise Permutation
     grp_levels <- levels(sub_g)
     obs_vals <- tapply(sub_x, sub_g, fun, ...)
     obs_vals <- obs_vals[grp_levels]
-    obs_diff <- abs(diff(obs_vals[grp_levels]))  # two-tailed
+    obs_diff <- abs(diff(obs_vals))  # two-tailed
 
-    null_diffs <-
-      apply_parallel(seq_len(R), function(i) {
+    null_diffs <- numeric(R)
+    n_valid <- 0L
+
+    n_invalid_total <- 0
+    n_generated_total <- 0
+
+    while (n_valid < R) {
+
+      batch_size <- R - n_valid
+
+      batch <- apply_parallel(seq_len(batch_size), function(i) {
+
         perm_g <- sample(sub_g)
         perm_vals <- tapply(sub_x, perm_g, fun, ...)
         perm_vals <- perm_vals[grp_levels]
-        abs(diff(perm_vals[grp_levels]))
+
+        if (any(!is.finite(perm_vals))) {
+          return(NA_real_)
+        } else {
+          d <- abs(diff(perm_vals))
+        }
+
+        if (!is.finite(d)) {
+          return(NA_real_)
+        } else {
+          d
+        }
+
       }, parallel = parallel, ncpus = ncpus, cl = cl)
+
+      batch <- unlist(batch)
+
+      n_generated_total <- n_generated_total + length(batch)
+      n_invalid_total   <- n_invalid_total + sum(!is.finite(batch))
+
+      # EARLY STOP CHECK
+      if (n_generated_total > 0) {
+        current_prop_invalid <- n_invalid_total / n_generated_total
+
+        if (current_prop_invalid > max.invalid) {
+          stop(
+            sprintf(
+              "Permutation test aborted: statistic function '%s' is not permutation-stable for comparison '%s' (%.1f%% invalid permutations, threshold = %.0f%%).",
+              deparse(substitute(fun)),
+              names(pw_results_list)[i],
+              100 * current_prop_invalid,
+              100 * max.invalid
+            ),
+            call. = FALSE
+          )
+        }
+      }
+
+      batch <- batch[is.finite(batch)]
+
+      if (length(batch) > 0) {
+        idx <- seq.int(from = n_valid + 1L,
+                       length.out = length(batch))
+        null_diffs[idx] <- batch
+        n_valid <- n_valid + length(batch)
+      }
+
+    }
+
+    # null_diffs <- null_diffs[seq_len(R)]
+
+    if (n_generated_total > 0) {
+      prop_invalid <- n_invalid_total / n_generated_total
+
+      if (prop_invalid > 0.02) {
+        warning(
+          sprintf(
+            "Function '%s' is not permutation-stable for comparison '%s' (%.1f%% invalid permutations).",
+            deparse(substitute(fun)),
+            names(pw_results_list)[i],
+            100 * prop_invalid
+          ),
+          call. = FALSE
+        )
+      }
+    }
 
     # phipson_permutation_2010
     p_val <- # mean(null_diffs >= obs_diff)
